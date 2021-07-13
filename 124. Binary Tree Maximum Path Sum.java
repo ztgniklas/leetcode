d347df5bf73e93242cc4d932e8848c84cbdd9f6a
taigezhang <i@taigezhang.com>
2021-07-13 13:58:40 +0800

    modify 124. Binary Tree Maximum Path Sum

--- 124. Binary Tree Maximum Path Sum.java
+++ 124. Binary Tree Maximum Path Sum.java
@@ -1,17 +1,5 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * 保存当前的最大值和一条分支的最大值
  */
 class Solution {
     public int maxPathSum(TreeNode root) {
@@ -29,5 +17,27 @@ class Solution {
         int curVal = root.val;
         int[] left = helper(root.left), right = helper(root.right);
         return new int[] {Math.max(curVal + right[1], Math.max(curVal + left[1], Math.max(curVal, Math.max(right[0], Math.max(curVal + left[1] + right[1], left[0]))))), Math.max(curVal + Math.max(left[1], right[1]), curVal)};
+    }
+}
+
+/**
+ * 更简洁的做法是维护全局的最大值，并且考虑到负值，分支的值要与0比大小
+ */
+class Solution {
+    int max;
+    public int maxPathSum(TreeNode root) {
+        max = Integer.MIN_VALUE;
+        helper(root);
+        return max;
+    }
+    
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        max = Math.max(max, left + right + root.val);
+        return root.val + Math.max(left, right);
     }
 }
